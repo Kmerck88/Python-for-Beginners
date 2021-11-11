b854304e3c369ad112f1a3c43dc2d531355e2a42
Kevin Merck <kevin.merck88@gmail.com>
2021-11-11 10:59:45 -0500

    Update main.py

--- Python for Beginners/main.py
+++ Python for Beginners/main.py
@@ -17,9 +17,9 @@ adj4 = input("Enter an adverb and hit enter: ")
 
 # our story
 story = 
-    f"Today I went to the zoo. I saw a {adj1} {noun1} jumping up and down in its tree. He {past_tense_verb} {adverb1} through the large tunnel that led to its {adj2} {noun2}."
+    (f"Today I went to the zoo. I saw a {adj1} {noun1} jumping up and down in its tree. He {past_tense_verb} {adverb1} through the large tunnel that led to its {adj2} {noun2}."
     f"got some peanuts and passed them through the cage to a gigantic gray {noun3} towering above my head. Feeding that animal made me hungry. I went to get a {adj3} scoop of ice cream. "
-    f"It filled my stomach. Afterwards I had to {verb} {adverb2}to catch our bus. When I got home I {adverb3} my mom for a {adverb4} day at the zoo"
+    f"It filled my stomach. Afterwards I had to {verb} {adverb2}to catch our bus. When I got home I {adverb3} my mom for a {adverb4} day at the zoo")
 
 # printing our story
 print(story)
