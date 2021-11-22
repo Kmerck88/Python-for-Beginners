4ec5fb2288474ab7ddf7348638192abad4bbc062
Kevin Merck <kevin.merck88@gmail.com>
2021-11-22 11:53:42 -0500

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,31 @@
+# WORK TWO 
+# Exercise One: Fahrenheit to Celsius
+
+
+# Fahrenheit = int(input("Please enter degrees in Fahrenheit:  "))
+
+# Celsius = (Fahrenheit - 32) * 5 / 9
+
+# print(Celsius)
+
+
+
+# Exercise Two: Exercise 2: Sales Tax
+
+
+# purchase_price = float(input("Please enter purchase price before taxes: "))
+
+# sales_tax= float(input("Please enter sales tax: "))
+
+# total_amount = (purchase_price + sales_tax) 
+
+
+    
+# print("The total cost after taxes is ${s:.2f}".format(s=total_amount)) 
+
+
+
+
+
+
+
