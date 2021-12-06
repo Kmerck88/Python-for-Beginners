65fe1d9a1f6ebdcd5b70a9b423cc95cb0bfc4003
Kevin Merck <kevin.merck88@gmail.com>
2021-12-06 13:24:19 -0500

    Create week_4.py

---
+++ week_4.py
@@ -0,0 +1,70 @@
+# Use a while loop to print all whole numbers from 1 to n
+# n = int(input("Please Enter Any Number: "))
+
+# i = 1
+# while(i<=n):
+#     print(i)
+#     i += 1
+
+# Use a while loop to print all whole numbers in reverse from n to 1
+# n = int(input("Please Enter any Number: "))
+# i = n
+
+# print("List of whole numbers from {0} to 1 in Reverse Order : ".format(n))
+
+# while (i >= 1):
+#     print (i, end = '  ')
+#     i = i - 1
+    
+# a = 65
+# while a <= 90: 
+#  b = chr(a)
+#  print("The Value of {} is alphabet {}.".format(a,b))
+#  a+=1 
+# else: 
+#     print("Learned my ABCs")
+
+
+# Use a while loop to print all even numbers between 1 and 100
+
+# i = 0 
+
+# while i <=100: 
+#     if i % 2 ==0: 
+#         print(i)
+#     i = i + 1
+
+
+
+# Use a while loop to print all odd numbers between 1 and 100
+
+# i =1
+
+# while i <= 100: 
+#     if i % 2 != 0: 
+#          print(i)
+#     i = i + 1
+
+
+
+# Use a while loop to print the sum of all the whole numbers from 1 - n
+
+# n = int(input("Please enter a number "))
+# i =1
+# sum = 0 
+
+# while i <= n: 
+#     sum = sum + i 
+#     i = i + 1
+# print(f"Sum of 1 to {n} whole number is:, {sum}")
+
+
+# Given the number n, use a while loop to print the multiplication table up to n.
+
+
+n = int(input("Please enter a number "))
+
+i =1
+while i <= 5: 
+   print(f"{n} * {i} =", n * i)
+   i=i+1
